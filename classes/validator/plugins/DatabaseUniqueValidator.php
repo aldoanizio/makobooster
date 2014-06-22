614956dff9ad5b12df76a944524fb6dd8ade0879
Aldo Anizio Lugão Camacho <aldoanizio@yahoo.com.br>
2014-06-22 17:18:11 -0400

    Fixed tab-whitespaces

--- classes/validator/plugins/DatabaseUniqueValidator.php
+++ classes/validator/plugins/DatabaseUniqueValidator.php
@@ -30,79 +30,79 @@ class DatabaseUniqueValidator extends \mako\validator\plugins\ValidatorPlugin im
 
     protected $packageName = 'makobooster';
 
-	/**
-	 * Rule name.
-	 *
-	 * @var string
-	 */
+    /**
+     * Rule name.
+     *
+     * @var string
+     */
 
-	protected $ruleName = 'unique';
+    protected $ruleName = 'unique';
 
-	/**
-	 * Connection manager instance.
-	 *
-	 * @var \mako\database\ConnectionManager
-	 */
+    /**
+     * Connection manager instance.
+     *
+     * @var \mako\database\ConnectionManager
+     */
 
-	protected $connectionManager;
+    protected $connectionManager;
 
     //---------------------------------------------
     // Class constructor, destructor etc ...
     //---------------------------------------------
 
-	/**
-	 * Constructor.
-	 *
-	 * @access  public
-	 * @param   \mako\database\ConnectionManager  $connectionManager  Connection manager instance
-	 */
+    /**
+     * Constructor.
+     *
+     * @access  public
+     * @param   \mako\database\ConnectionManager  $connectionManager  Connection manager instance
+     */
 
-	public function __construct(ConnectionManager $connectionManager)
-	{
-		$this->connectionManager = $connectionManager;
-	}
+    public function __construct(ConnectionManager $connectionManager)
+    {
+        $this->connectionManager = $connectionManager;
+    }
 
     //---------------------------------------------
     // Class methods
     //---------------------------------------------
 
-	/**
-	 * Validator.
-	 *
-	 * @access  public
-	 * @param   string   $input       Input
-	 * @param   array    $parameters  Parameters
-	 * @return  boolean
-	 */
+    /**
+     * Validator.
+     *
+     * @access  public
+     * @param   string   $input       Input
+     * @param   array    $parameters  Parameters
+     * @return  boolean
+     */
 
-	public function validate($input, $parameters)
-	{
-		$query = $this->connectionManager->builder()->table($parameters[0])->where($parameters[1], '=', $input);
+    public function validate($input, $parameters)
+    {
+        $query = $this->connectionManager->builder()->table($parameters[0])->where($parameters[1], '=', $input);
 
-		// Ignore a given value
+        // Ignore a given value
 
-		if(isset($parameters[2]) && ($parameters[2] !== 'NULL'))
-		{
-			$query->where($parameters[1], '!=', $parameters[2]);
-		}
+        if(isset($parameters[2]) && ($parameters[2] !== 'NULL'))
+        {
+            $query->where($parameters[1], '!=', $parameters[2]);
+        }
 
-		// Additional clauses
+        // Additional clauses
 
-		for($i = 3; $i < count($parameters); $i++)
-		{
-			// Explode clauses
+        for($i = 3; $i < count($parameters); $i++)
+        {
+            // Explode clauses
 
-			list($field, $clauses) = explode('(', $parameters[$i], 2);
+            list($field, $clauses) = explode('(', $parameters[$i], 2);
 
-			// Explode clauses
+            // Explode clauses
 
-			list($conditional, $value) = explode(':', rtrim($clauses, ')'), 2);
+            list($conditional, $value) = explode(':', rtrim($clauses, ')'), 2);
 
-			// Perform query
+            // Perform query
 
-			$query->where($field, $conditional, $value);
-		}
+            $query->where($field, $conditional, $value);
+        }
 
-		return ($query->count() == 0);
-	}
+        return ($query->count() == 0);
+    }
 }
